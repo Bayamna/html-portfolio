4fbd032dd603390c2677cbe1ba8f372324a8488e
Bayamna <50969582+Bayamna@users.noreply.github.com>
2025-05-12 16:15:28 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -7,4 +7,4 @@ document.querySelector(".back-icon").addEventListener("click", function(){
     document.querySelector(".booking-form").classList.toggle("form-visible");
     document.querySelector(".blur").classList.toggle("active");
 })
-
+ 
