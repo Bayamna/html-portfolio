1935ba3f36710943159a532ce77a3e489469268f
Bayamna <50969582+Bayamna@users.noreply.github.com>
2025-05-13 12:33:32 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.querySelector(".booking-form-btn").addEventListener("click",function(){
-    document.querySelector(".blur").classList.toggle("active");
-    document.querySelector(".booking-form").classList.toggle("form-visible");
+document.querySelector(".menu-icon").addEventListener("click", function(){
+    document.querySelector(".toggled-navigation-bar").classList.toggle("visible");
 })
 
-document.querySelector(".back-icon").addEventListener("click", function(){
-    document.querySelector(".booking-form").classList.toggle("form-visible");
-    document.querySelector(".blur").classList.toggle("active");
+document.addEventListener("click", function(event){
+    var navigation = document.querySelector(".toggled-navigation-bar");
+    if(event.target == navigation){
+        document.querySelector(".toggled-navigation-bar").classList.toggle("visible");
+    }
 })
- 
