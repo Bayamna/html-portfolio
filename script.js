6e5a78255e91192542ab401f2b6eac78f9edda88
Bayamna <50969582+Bayamna@users.noreply.github.com>
2025-05-15 11:35:14 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ $(".menu-icon").click(function(){
     $(".toggled-navigation-bar").addClass("visible");
 })
 
-
-
 var menu_modal = document.querySelector(".toggled-navigation-bar");
 document.addEventListener("click", function(event){
     if(event.target == menu_modal){
