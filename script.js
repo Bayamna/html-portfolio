b342e8cee5cb5b4318722eca37a213855a3a6891
Bayamna <50969582+Bayamna@users.noreply.github.com>
2025-05-15 17:14:06 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-$(".menu-icon").click(function(){
-    $(".toggled-navigation-bar").slideDown();
-    $(".toggled-navigation-bar").addClass("visible");
+
+
+$(document).ready(function(){
+    // OPEN DROP-DOWN MENU
+    $(".menu-icon").click(function(){
+        $(".toggled-navigation-bar").slideDown();
+        $(".toggled-navigation-bar").addClass("visible");
+    })
+    
+    // CLOSE DROP-DOWN MENU
+    var menu_modal = document.querySelector(".toggled-navigation-bar");
+    document.addEventListener("click", function(event){
+        if(event.target == menu_modal){
+            menu_modal.style.display = "none";
+        }
+    })
+    
+    // OPEN BOOKING-FORM
+    document.querySelector(".booking-btn").addEventListener("click", function(){
+        $(".booking-form-container").slideDown("slow");
+        document.querySelector(".booking-form-container").classList.add("visible");
+        document.querySelector("body").classList.add("active-overflow");
+    })
+    
+    // CLOSE BOOKING-FORM
+    document.querySelector(".form-btn").addEventListener("click", function(){
+        $(".booking-form-container").fadeOut("slow");
+        document.querySelector(".booking-form-container").classList.remove("visible")
+    })    
 })
 
-var menu_modal = document.querySelector(".toggled-navigation-bar");
-document.addEventListener("click", function(event){
-    if(event.target == menu_modal){
-        menu_modal.style.display = "none";
-    }
-})
-
-document.querySelector(".booking-btn").addEventListener("click", function(){
-    $(".booking-form-container").slideDown("slow");
-    document.querySelector(".booking-form-container").classList.add("visible")
-})
