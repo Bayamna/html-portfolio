01713076f0fbf7dac6ba5d3121a62803e9daa63a
Bayamna <50969582+Bayamna@users.noreply.github.com>
2025-05-19 14:11:42 +0000

    Add files via upload

--- header-footer.js
+++ header-footer.js
@@ -2,17 +2,17 @@ class MyHeader extends HTMLElement{
     connectedCallback(){
         this.innerHTML = 
         
-        <header>
+        `<header>
             <nav class="top-navigation-bar">
                 <a href="index.html"> <img src="images/logo.png" alt="company-logo" class="logo" /></a>
                 <ul class="top-navigation-links">
-                    <li><a href="about.html" class="top-navigation-items">About</a></li>
-                    <li><a href="services.html" class="top-navigation-items">Services</a></li>
-                    <li><a href="#" class="top-navigation-items">Products</a></li>
-                    <li><a href="#" class="top-navigation-items">Reviews</a></li>
-                    <li><a href="#" class="top-navigation-items">Blog</a></li>
-                    <li><a href="#" class="top-navigation-items">FAQ</a></li>
-                    <li><a href="#" class="top-navigation-items">Booking</a></li>
+                    <li><a href="about.html" class="top-navigation-link">About</a></li>
+                    <li><a href="services.html" class="top-navigation-link">Services</a></li>
+                    <li><a href="#" class="top-navigation-link">Products</a></li>
+                    <li><a href="#" class="top-navigation-link">Reviews</a></li>
+                    <li><a href="#" class="top-navigation-link">Blog</a></li>
+                    <li><a href="#" class="top-navigation-link">FAQ</a></li>
+                    <li><a href="#" class="top-navigation-link">Booking</a></li>
                 </ul>
                 <img src="images/menu-icon.png" alt="menu-icon" class="menu-icon" />
 
@@ -27,13 +27,13 @@ class MyHeader extends HTMLElement{
                  <div class="toggled-navigation-bar">
                     <div class="toggled-nav-items">
                         <ul class="toggled-navigation-links">
-                            <li><a href="#" class="toggled-navigation-items">About</a></li>
-                            <li><a href="#" class="toggled-navigation-items">Services</a></li>
-                            <li><a href="#" class="toggled-navigation-items">Products</a></li>
-                            <li><a href="#" class="toggled-navigation-items">Reviews</a></li>
-                            <li><a href="#" class="toggled-navigation-items">Blog</a></li>
-                            <li><a href="#" class="toggled-navigation-items">FAQ</a></li>
-                            <li><a href="#" class="toggled-navigation-items">Booking</a></li>
+                            <li><a href="#" class="toggled-navigation-link">About</a></li>
+                            <li><a href="#" class="toggled-navigation-link">Services</a></li>
+                            <li><a href="#" class="toggled-navigation-link">Products</a></li>
+                            <li><a href="#" class="toggled-navigation-link">Reviews</a></li>
+                            <li><a href="#" class="toggled-navigation-link">Blog</a></li>
+                            <li><a href="#" class="toggled-navigation-link">FAQ</a></li>
+                            <li><a href="#" class="toggled-navigation-link">Booking</a></li>
                         </ul>
 
                         <ul class="toggled-social-media">
@@ -44,9 +44,31 @@ class MyHeader extends HTMLElement{
                         </ul>
                     </div>
                 </div>
-        </header>
-        
+        </header>`
     }
 }
 
 customElements.define('special-header', MyHeader)
+
+class MyFooter extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML =
+        
+        `<footer class="footer-wrapper">
+            <div class="footer-items">
+                <p class="copyright-text">Curves Republic Ltd. © 2025</p>
+                <div class="contacts">
+                    <div class="contact-icon-background">
+                        <img src="images/call.png" alt="call-icon" class="contact-icons">
+                    </div>
+                    <div class="contact-icon-background">
+                        <img src="images/whatsapp.png" alt="whatsapp-icon" class="contact-icons" >
+                    </div>
+                    <p class="contact-numbers">024 378 3130 - 020 431 8170</p>
+                </div>
+            </div>
+        </footer>`
+    }
+
+}
+customElements.define('special-footer', MyFooter)
